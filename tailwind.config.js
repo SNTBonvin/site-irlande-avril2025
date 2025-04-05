dff082aab834cd88a8611cfa254185b49584ef4d
Bonvin SNT <60291789+SNTBonvin@users.noreply.github.com>
2025-04-05 18:25:00 +0200

    Thème basique et deux articles page cassée pas de thème sombre

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+export default {
+    darkMode: 'class',
+    content: ['./src/**/*.{astro,js,ts,jsx,tsx}'],
+    safelist: [
+      'dark',
+      'dark:bg-gray-900',
+      'dark:text-white',
+      'dark:bg-gray-800',
+      'dark:text-gray-300',
+      'dark:text-green-300',
+      'grid-cols-1',
+      'sm:grid-cols-2',
+      'md:grid-cols-3',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
